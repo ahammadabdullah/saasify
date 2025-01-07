573ee44b932d3d3cff94d48a613f447226c7951d
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2025-01-08 00:59:47 +0600

    [005] email templates functionality

--- src/app/(dashboard)/emails/page.tsx
+++ src/app/(dashboard)/emails/page.tsx
@@ -171,7 +171,7 @@ export default function EmailsPage() {
                     {!isSending ? (
                       "Test"
                     ) : (
-                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                      <Icons.spinner className="h-4 w-4 animate-spin" />
                     )}
                   </Button>
                 </div>
